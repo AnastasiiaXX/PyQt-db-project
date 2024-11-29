91a9f3044e96c0e9a2d7e58f676db8d587ccd40a
Anastasiia Kononova <bjko.smsng@gmail.com>
2024-11-29 08:00:59 +0300

    update method in progress

--- src/Teacher.py
+++ src/Teacher.py
@@ -15,6 +15,32 @@ class Model(QSqlQueryModel):
             '''
         self.setQuery(sql)
 
+    def add(self, fio, phone, email, comnt):
+        add_query = QSqlQuery()
+        INSERT = '''
+                    insert into teachers (fio, phone, email, comnt)
+                    values ( :fio, :phone, :email, :comnt );
+                '''
+        add_query.prepare(INSERT)
+        add_query.bindValue(':fio', fio)
+        add_query.bindValue(':phone', phone[:10])
+        add_query.bindValue(':email', email)
+        add_query.bindValue(':comnt', comnt)
+        add_query.exec_()
+        self.refresh()
+
+    def select_one(self, id_teacher):
+        sel_query = QSqlQuery()
+        SELECT_ONE = '''
+            select fio, phone, email, comnt
+            from teachers 
+            where id_teacher = :id_teacher;
+        '''
+        sel_query.prepare(SELECT_ONE)
+        sel_query.bindValue(':id_teacher', id_teacher)
+        sel_query.exec()
+        if sel_query
+
     def update(self, fio, phone, email, comnt, id_teacher):
         update_query = QSqlQuery()
         UPDATE = '''
@@ -27,38 +53,11 @@ class Model(QSqlQueryModel):
         '''
         update_query.prepare(UPDATE)
         update_query.bindValue(':fio', fio)
-        update_query.bindValue(':phone', phone)
+        update_query.bindValue(':phone', phone[:10])
         update_query.bindValue(':email', email)
         update_query.bindValue(':comnt', comnt)
         update_query.bindValue(':id_teacher', id_teacher)
-        ans = update_query.exec()
-        print(ans, update_query.lastQuery())
-        update_query.clear()
-        self.refresh()
-
-    def get_teacher(self, id_teacher):
-        get_query = QSqlQuery()
-        sql = '''
-            select fio, phone, email, comnt
-            from teachers where id_teacher = :id_teacher
-        '''
-        get_query.prepare(sql)
-        get_query.bindValue(':id_teacher', id_teacher)
-        get_query.exec()
-
-    def add(self, fio, phone, email, comnt):
-        add_query = QSqlQuery()
-        INSERT = '''
-                    insert into teachers (fio, phone, email, comnt)
-                    values ( :fio, :phone, :email, :comnt );
-                '''
-        add_query.prepare(INSERT)
-        add_query.bindValue(':fio', fio)
-        add_query.bindValue(':phone', phone[:10])
-        add_query.bindValue(':email', email)
-        add_query.bindValue(':comnt', comnt)
-        ans = add_query.exec_()
-        add_query.clear()
+        update_query.exec_()
         self.refresh()
 
 class View(QTableView):
@@ -79,7 +78,7 @@ class View(QTableView):
         dialog = Dialog(parent=self)
         row = self.currentIndex().row()
         id_teacher = self.model().record(row).value(0)
-        dialog.fio, dialog.phone, dialog.email, dialog.comnt = self.model().get_teacher(id_teacher)
+        dialog.fio, dialog.phone, dialog.email, dialog.comnt = self.model().select_one(id_teacher)
         if dialog.exec():
             self.model().update(dialog.fio, dialog.phone, dialog.email, dialog.comnt, id_teacher)
 
